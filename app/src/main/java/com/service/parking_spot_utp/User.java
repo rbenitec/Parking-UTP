172432fbb6bec67576632c2d1b2b17105ec96a01
RoxiM16 <roxi160803@gmail.com>
2024-06-04 19:32:45 -0500

    vista success

--- app/src/main/java/com/service/parking_spot_utp/User.java
+++ app/src/main/java/com/service/parking_spot_utp/User.java
@@ -1,44 +1,33 @@
 package com.service.parking_spot_utp;
-
 public class User {
     public String email;
     public String password;
     public String token;
     public String placa1;
-
-
     public String getEmail() {
         return email;
     }
-
     public void setEmail(String email) {
         this.email = email;
     }
-
     public String getPassword() {
         return password;
     }
-
     public void setPassword(String password) {
         this.password = password;
     }
-
     public String getToken() {
         return token;
     }
-
     public void setToken(String token) {
         this.token = token;
     }
-
     public String getPlaca1() {
         return placa1;
     }
-
     public void setPlaca1(String placa1) {
         this.placa1 = placa1;
     }
-
 
     public User(String email, String password, String token, String placa1) {
         this.email = email;
